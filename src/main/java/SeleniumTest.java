6d1a45e8c87db65113f8e0b5c2442d9a4e560608
jozef.radic <jozef.radic@akademiasovy.sk>
2019-03-25 13:38:11 +0100

    Added 3 classes

--- src/main/java/SeleniumTest.java
+++ src/main/java/SeleniumTest.java
@@ -1,47 +1,20 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.concurrent.TimeUnit;
 
-
-
-public class SeleniumTest {
+public class SeleniumTest extends FunctionalTest {
 
     @Test
     public void Test(){
+        driver.get("https://www.mortgageloan.com/calculator");
 
-        System.setProperty("webdriver.chrome.driver","src\\main\\java\\chromedriver.exe");
-        WebDriver chromeDriver = new ChromeDriver();
+        MortgageloanCalculator mortgageloanCalculator = new MortgageloanCalculator(driver);
+        mortgageloanCalculator.firstPage("200,000","5.0","30","235,000");
 
-        chromeDriver.get("https://www.mortgageloan.com/calculator");
-        chromeDriver.manage().window().maximize();
-        chromeDriver.findElement(By.id("calculator_widget_amount")).click();
-        chromeDriver.findElement(By.id("calculator_widget_amount")).sendKeys("200000");
-        chromeDriver.findElement(By.id("calculator_widget_interest")).click();
-        chromeDriver.findElement(By.id("calculator_widget_interest")).sendKeys("5.0");
-        chromeDriver.findElement(By.id("calculator_widget_Length")).click();
-        chromeDriver.findElement(By.id("calculator_widget_Length")).sendKeys("30");
-        chromeDriver.findElement(By.id("calculator_widget_HomeValue")).click();
-        chromeDriver.findElement(By.id("calculator_widget_HomeValue")).sendKeys("235000");
-        chromeDriver.findElement(By.xpath("//*[@id=\"wizard-pager\"]/li[2]/a")).click();
-        chromeDriver.findElement(By.id("calculator_widget_PropertyTaxes")).click();
-        chromeDriver.findElement(By.id("calculator_widget_PropertyTaxes")).sendKeys("2000");
-        chromeDriver.findElement(By.id("calculator_widget_Insurance")).click();
-        chromeDriver.findElement(By.id("calculator_widget_Insurance")).sendKeys("1865");
-        chromeDriver.findElement(By.id("calculator_widget_PMI")).click();
-        chromeDriver.findElement(By.id("calculator_widget_PMI")).sendKeys("0.52");
-        chromeDriver.findElement(By.xpath("//*[@id=\"wizard-pager\"]/li[5]/a")).click();
+        mortgageloanCalculator.secondPage("2,000","1,865","0.52");
 
-        chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        String result= chromeDriver.findElement(By.xpath("//th[contains(text(),'Monthly Principal & Interests')]/following-sibling::td")).getText();
-        Assert.assertEquals(result,"$1,073.64");
-        String result2= chromeDriver.findElement(By.xpath("//th[contains(text(),'Loan To Value Ratio')]/following-sibling::td")).getText();
-        Assert.assertEquals(result2,"85.11%");
-        String result3= chromeDriver.findElement(By.xpath("//th[contains(text(),'Total Monthly Payments')]/following-sibling::td")).getText();
-        Assert.assertEquals(result3,"$1,482.39");
+        Assert.assertEquals(mortgageloanCalculator.result().get(0),"$1,073.64");
+        Assert.assertEquals(mortgageloanCalculator.result().get(1),"$1,482.39");
+        Assert.assertEquals(mortgageloanCalculator.result().get(2),"85.11%");
 
     }
 }
